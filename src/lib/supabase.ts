ed753d77740d3d6c740c517a768a7cdedbb72c6b
mohsenfarsi <51865631+mohsenfarsi@users.noreply.github.com>
2025-06-05 03:31:55 +0330

    Add files via upload
    15 Khordad - 03:31

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ddsfemupgawvspyscggd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkc2ZlbXVwZ2F3dnNweXNjZ2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNzc0MzUsImV4cCI6MjA2NDY1MzQzNX0.Rznvz9jLm7DOIa5ENp86lrW5NMBeOAfxHQkBV-fW-3Q';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
